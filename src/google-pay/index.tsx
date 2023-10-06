8e312f3c46a6c545f78400131ca00045cef0aed8
jayk9601 <maxthonpc18@gmail.com>
2023-10-06 09:48:33 +0530

    google pay implement

--- src/google-pay/index.tsx
+++ src/google-pay/index.tsx
@@ -1,11 +1,28 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {
+  GooglePayButton,
+  onGooglePayment,
+  onApplePay,
+} from 'react-apple-google-pay';
 
 const GooglePay = () => {
   return (
-    <View>
-      <Text>GooglePay</Text>
-    </View>
+    <GooglePayButton
+      title="on Payment"
+      onPress={() => {
+        onApplePay({
+          amount: '0.01',
+          merchantId: 'acct_1MKaf5SJHee3mOwM',
+          paymentLabel: 'Test',
+        })
+          .then(res => {
+            console.log('res : ', res);
+          })
+          .catch(err => {
+            console.log('err : ', err);
+          });
+      }}
+    />
   );
 };
 
