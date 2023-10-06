b8cf00033fc4c7ccc3dcb04db9009e1f8340bc7b
jayk9601 <maxthonpc18@gmail.com>
2023-10-06 18:21:13 +0530

    add radio button

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Button, RNText} from 'theme-component';
+import {Text, View} from 'react-native';
+import {Button, RNText, RadioButtonGroup} from 'theme-component';
 
 const Testing = () => {
   return (
     <View>
       <Button label="test" />
       <RNText label="test" />
+      <RadioButtonGroup
+        data={[
+          {
+            title: 'string',
+            value: 's',
+          },
+        ]}
+        onSelect={e => {
+          console.log(e);
+        }}
+      />
     </View>
   );
 };
